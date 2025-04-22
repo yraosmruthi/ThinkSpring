ed35ae0ac6637babba4b42c13430bb59e5734b11
yraosmruthi <yraosmruthi@gmail.com>
2025-04-22 10:20:37 +0530

    Made login and register functionality

--- server/server.js
+++ server/server.js
@@ -5,17 +5,24 @@ const app = express();
 const connectDb = require('./utils/db');
 const authrouter = require('./routes/auth-route');
 const contactRoute = require('./routes/contact-route')
+const cors = require('cors');
 
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods:'GET,POST,PUT,DELETE,PATCH,HEAD',
+    credentials:true 
+  }
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/auth",authrouter);
 app.use("/form",contactRoute)
 
+
 app.get("/",(req,res)=>{
     res.send('hello');
 })
-
-
 
 const PORT = 3000;
 
